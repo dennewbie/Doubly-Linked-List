59df58ead4d4573991e1583662b2566299379d89
DC <caruso.denny@gmail.com>
2020-08-31 10:18:45 +0200

    Initial Commit

---
+++ stackDoublyLinkedList/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  stackDoublyLinkedList
+//
+//  Created by Denny Caruso on 31/08/2020.
+//  Copyright © 2020 Denny Caruso. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
